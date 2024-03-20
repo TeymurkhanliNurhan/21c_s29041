4fef580cce69c97a1afeee8efd0a7c01f1ef8855
TeymurkhanliNurhan <s29041@pjwstk.edu.pl>
2024-03-20 23:53:10 +0100

    created a function which does task1

--- PPY3/main.py
+++ PPY3/main.py
@@ -2,4 +2,11 @@
 import math
 
 squares=[x**2 for x in range(1,11)]
-print(squares)
+print("task1: ",squares)
+
+#task2
+def squareFunc(start, end):
+    squares2 = [x ** 2 for x in range(start, end )]
+    return squares2
+
+print("task2: ",squareFunc(1,11))
